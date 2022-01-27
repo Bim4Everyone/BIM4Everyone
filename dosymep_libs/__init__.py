1f90be28514e04f8136bc5b362f88eb34b4e0065
dosymep <biseuv@yandex.ru>
2022-01-27 14:45:13 +0300

    Перенес инициализацию платформы в startup.py

--- dosymep_libs/__init__.py
+++ dosymep_libs/__init__.py
@@ -5,6 +5,7 @@ import sys
 import os.path as op
 from pyrevit import HOST_APP
 
+
 def load_assemblies():
     directory = op.dirname(__file__)
     framework_path = os.path.join(directory, HOST_APP.version)
