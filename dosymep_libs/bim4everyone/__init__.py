28ceb0e34bca496f655fa281b959b36c03dd32c1
dosymep <dosymep@gmail.com>
2022-04-05 12:45:45 +0300

    Добавил декоратор для логгирования скриптов

---
+++ dosymep_libs/bim4everyone/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+import clr
+import traceback
+
+from dosymep_libs.simple_services import *
+
+def log_plugin(plugin_name):
+    def plugin_decorator(func):
+        def wrapper(*args, **kwargs):
+            plugin_logger_service = get_logger_service().ForPluginContext(plugin_name)
+            plugin_logger_service.Information("Запуск команды расширения.")
+            try:
+                func(plugin_logger_service, *args, **kwargs)
+                plugin_logger_service.Information("Выход из команды расширения.")
+            except Exception as ex:
+                plugin_logger_service.Warning("IronPython Traceback.\r\n" + traceback.format_exc().strip())
+                raise
+
+        return wrapper
+
+    return plugin_decorator
