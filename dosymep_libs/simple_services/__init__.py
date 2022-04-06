47c148b0431213c12b2a10ecc78f358c84d5631d
dosymep <dosymep@gmail.com>
2022-04-06 14:50:43 +0300

    Добавил дополнительные методы отображение уведомлений для скриптов

--- dosymep_libs/simple_services/__init__.py
+++ dosymep_libs/simple_services/__init__.py
@@ -10,6 +10,12 @@ clr.AddReference('dosymep.SimpleServices.dll')
 clr.AddReference('Serilog.dll')
 clr.AddReference('Serilog.Sinks.File.dll')
 
+from System import Uri
+from System.Windows.Media.Imaging import BitmapImage
+
+from pyrevit import script
+from pyrevit import EXEC_PARAMS
+
 from dosymep.SimpleServices import *
 from dosymep.Bim4Everyone.SimpleServices import *
 
@@ -25,3 +31,16 @@ def get_notification_service():
 def show_notification_service(title, body, footer=None, image_source=None):
     notification_service = get_notification_service()
     notification_service.CreateNotification(title, body, footer, image_source).ShowAsync()
+
+
+def show_canceled_script_notification():
+    show_script_notification("Выполнение скрипта отменено.")
+
+
+def show_executed_script_notification():
+    show_script_notification("Выполнение скрипта завершено успешно.")
+
+
+def show_script_notification(body):
+    show_notification_service(script.get_button().ui_title, body,
+                              image_source=BitmapImage(Uri(EXEC_PARAMS.command_path + "\icon.png")))
