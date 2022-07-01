7e898e3217af742b818f4b88f6d49c626b44f530
dosymep <dosymep@gmail.com>
2022-07-01 16:01:19 +0300

    Обновил пути до библиотек фреймворка

--- dosymep_libs/__init__.py
+++ dosymep_libs/__init__.py
@@ -2,11 +2,29 @@
 
 import os
 import sys
+import shutil
 import os.path as op
+
 from pyrevit import HOST_APP
 
 
+def copy_files(source_dir, target_dir):
+    for file_name in os.listdir(source_dir):
+        source = op.join(source_dir, file_name)
+        target = op.join(target_dir, file_name)
+        if os.path.isfile(source):
+            shutil.copy(source, target)
+
+
 def load_assemblies():
-    directory = op.dirname(__file__)
-    framework_path = os.path.join(directory, HOST_APP.version)
-    sys.path.append(framework_path)
+    temp_path = os.path.join(os.getenv('TEMP'), "Bim4Everyone", "dosymep")
+    if not op.exists(temp_path):
+        app_libs = op.join(op.dirname(__file__), "libs")
+
+        version_libs = op.join(app_libs, HOST_APP.version)
+        shutil.copytree(version_libs, temp_path)
+        copy_files(app_libs, temp_path)
+
+    sys.path.append(temp_path)
+
+
