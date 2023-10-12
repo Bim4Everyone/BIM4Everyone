4ec9a09b61cb1ea0f3af47c775510ac8e1e639ee
dosymep <dosymep@gmail.com>
2023-10-12 11:24:36 +0300

    Порефакторил скрипты

--- pySpeech/ViewSheets/__init__.py
+++ pySpeech/ViewSheets/__init__.py
@@ -23,13 +23,6 @@ from pyrevit import script
 from pyrevit import revit
 from pyrevit import forms
 
-def alert(msg, exit_script=True):
-    if msg:
-        print msg
-
-    if exit_script:
-        script.exit()
-
 
 def get_table_columns():
     return ["SheetId", "SheetName"]
@@ -51,7 +44,8 @@ def show_error_sheets(title, sheets):
 
         table_columns = get_table_columns()
         table_data = get_table_data(output, sheets)
-        show_table(output, title, "Виды к которых совпадает атрибут \"Номер листа\".", table_columns, table_data)
+        show_table(output, title,
+                   "Виды к которых совпадает атрибут \"Номер листа\".", table_columns, table_data)
 
 
 def show_table(output, title, table_title, table_columns, table_data, exit_script=True):
@@ -62,8 +56,8 @@ def show_table(output, title, table_title, table_columns, table_data, exit_scrip
 
 
 class DocumentRepository(object):
-    def __init__(self, uiApplication):
-        self.UIApplication = uiApplication
+    def __init__(self, ui_application):
+        self.UIApplication = ui_application
 
     @property
     def Application(self):
@@ -71,151 +65,136 @@ class DocumentRepository(object):
 
     @property
     def Document(self):
-        return self.UIDocument.Document
+        return self.ui_document.Document
 
     @property
-    def UIDocument(self):
+    def ui_document(self):
         return self.UIApplication.ActiveUIDocument
 
-    def GetViewSheets(self):
+    def get_view_sheets(self):
         return FilteredElementCollector(self.Document).OfClass(ViewSheet).ToElements()
 
-    def GetSelectedViewSheets(self):
-        return filter(lambda x: isinstance(x, ViewSheet), self.UIDocument.GetSelectedElements())
+    def get_selected_view_sheets(self):
+        return filter(lambda x: isinstance(x, ViewSheet), self.ui_document.GetSelectedElements())
 
 
 class ViewSheetModel(object):
-    def __init__(self, index, viewSheet):
+    def __init__(self, index, view_sheet):
         self.__index = index
-        self.__viewSheet = viewSheet
+        self.__viewSheet = view_sheet
 
     @property
     def Id(self):
-        return  self.__viewSheet.Id
+        return self.__viewSheet.Id
 
     @property
-    def SheetAlbum(self):
-        return ViewSheetModel.GetSheetAlbum(self.__viewSheet)
+    def sheet_album(self):
+        return ViewSheetModel.get_sheet_album(self.__viewSheet)
 
     @property
-    def SheetIndex(self):
+    def sheet_index(self):
         return self.__index
 
-    # @property.setter
-    # def SheetIndex(self, value):
-    #     if isinstance(value, int):
-    #         self.__index = value
-
     @property
-    def SheetName(self):
-        if self.SheetAlbum:
-            return "{}-{}".format(self.SheetAlbum, self.__index)
+    def sheet_name(self):
+        if self.sheet_album:
+            return "{}-{}".format(self.sheet_album, self.__index)
 
         return str(self.__index)
 
     @property
-    def OriginalSheetIndex(self):
-        return int(self.OriginalSheetName.split("-").pop())
+    def original_sheet_index(self):
+        return int(self.original_sheet_name.split("-").pop())
 
     @property
-    def OriginalSheetName(self):
+    def original_sheet_name(self):
         return self.__viewSheet.GetParamValueOrDefault(BuiltInParameter.SHEET_NUMBER)
 
-    def UpdateName(self):
+    def update_name(self):
         self.__viewSheet.SetParamValue(SharedParamsConfig.Instance.StampSheetNumber, str(self.__index))
-        self.__viewSheet.SetParamValue(BuiltInParameter.SHEET_NUMBER, self.SheetName)
+        self.__viewSheet.SetParamValue(BuiltInParameter.SHEET_NUMBER, self.sheet_name)
 
-    def UpdateUniqueName(self):
+    def update_unique_name(self):
         self.__viewSheet.SetParamValue(BuiltInParameter.SHEET_NUMBER, str(Guid.NewGuid()))
 
     @staticmethod
-    def GetSheetAlbum(view_sheet):
+    def get_sheet_album(view_sheet):
         complect_blueprints = view_sheet.GetParamValueOrDefault(SharedParamsConfig.Instance.AlbumBlueprints)
         return complect_blueprints if complect_blueprints else ""
 
 
 class OrderViewSheetModel(object):
-    def __init__(self, documentRepository, start_index=1):
-        self.DocumentRepository = documentRepository
+    def __init__(self, document_repository, start_index=1):
+        self.ViewSheets = []
         self.StartIndex = start_index
+        self.DocumentRepository = document_repository
 
-    def OrderViewSheets(self):
+    def order_view_sheets(self):
         project_parameters = ProjectParameters.Create(self.DocumentRepository.Application)
-        project_parameters.SetupRevitParams(self.DocumentRepository.Document, SharedParamsConfig.Instance.AlbumBlueprints,
+        project_parameters.SetupRevitParams(self.DocumentRepository.Document,
+                                            SharedParamsConfig.Instance.AlbumBlueprints,
                                             SharedParamsConfig.Instance.StampSheetNumber)
 
-        with TransactionGroup(self.DocumentRepository.Document) as transaction:
-            transaction.Start("Перенумерация листов")
-
+        with revit.TransactionGroup("BIM: Перенумерация листов") as transaction:
             # переименовываем листы на случайные имена
             # чтобы случайно не пересеклись новые имена со старыми
-            self.UpdateUniqueNames()
-            self.UpdateNames()
+            self.update_unique_names()
+            self.update_names()
 
-            transaction.Assimilate()
-
-    def CheckUniquesNames(self):
+    def check_uniques_names(self):
         sheet_ids = [sheet.Id for sheet in self.ViewSheets]
-        sheets = filter(lambda x: x.Id not in sheet_ids, self.GetViewSheets())
+        sheets = filter(lambda x: x.Id not in sheet_ids, self.get_view_sheets())
 
-        sheet_names = [sheet.SheetName for sheet in self.ViewSheets]
+        sheet_names = [sheet.sheet_name for sheet in self.ViewSheets]
         error_sheets = filter(lambda x: x.SheetNumber in sheet_names, sheets)
 
         show_error_sheets("Ошибка", error_sheets)
 
-    def UpdateNames(self):
-        with Transaction(self.DocumentRepository.Document) as transaction:
-            transaction.Start("Нумерация")
-
+    def update_names(self):
+        with revit.Transaction("BIM: Нумерация"):
             for view_sheet in self.ViewSheets:
-                view_sheet.UpdateName()
+                view_sheet.update_name()
 
-            transaction.Commit()
-
-    def UpdateUniqueNames(self):
-        with Transaction(self.DocumentRepository.Document) as transaction:
-            transaction.Start("Нумерация")
-
+    def update_unique_names(self):
+        with revit.Transaction("BIM: Нумерация"):
             for view_sheet in self.ViewSheets:
-                view_sheet.UpdateUniqueName()
+                view_sheet.update_unique_name()
 
-            transaction.Commit()
+    def get_view_sheets(self):
+        return self.DocumentRepository.get_view_sheets()
 
-    def GetViewSheets(self):
-        return self.DocumentRepository.GetViewSheets()
+    def get_selected_view_sheet(self):
+        selections = self.DocumentRepository.get_selected_view_sheets()
+        selections = [view for view in selections if view.ViewType == ViewType.DrawingSheet]
+        return OrderViewSheetModel.check_sheets(selections)
 
-    def GetSelectedViewSheet(self):
-        selections = self.DocumentRepository.GetSelectedViewSheets()
-        return OrderViewSheetModel.CheckSheets(selections)
+    def load_view_sheets(self):
+        selected_view_sheet = self.get_selected_view_sheet()[0]
 
-    def LoadViewSheets(self):
-        selected_view_sheet = self.GetSelectedViewSheet()[0]
+        view_album = ViewSheetModel.get_sheet_album(selected_view_sheet)
+        self.set_view_sheets([sheet for sheet in self.get_view_sheets() if ViewSheetModel.get_sheet_album(sheet) == view_album])
 
-        view_album = ViewSheetModel.GetSheetAlbum(selected_view_sheet)
-        self.SetViewSheets([sheet for sheet in self.GetViewSheets() if ViewSheetModel.GetSheetAlbum(sheet) == view_album])
+    def load_selected_view_sheets(self):
+        self.set_view_sheets(self.get_selected_view_sheet())
 
-    def LoadSelectedViewSheets(self):
-        self.SetViewSheets(self.GetSelectedViewSheet())
-
-    def SetViewSheets(self, sheets):
+    def set_view_sheets(self, sheets):
         sheets = sorted(sheets, cmp=ViewSheetComparer().Compare)
         self.ViewSheets = [ViewSheetModel(index + self.StartIndex, view_sheet) for index, view_sheet in enumerate(sheets)]
 
     @staticmethod
-    def CheckSheets(selections):
+    def check_sheets(selections):
         if not selections:
-            alert("Не были выбраны элементы.")
+            forms.alert("Не были выбраны листы.", exitscript=True)
 
-        sheet_numbers = set([ViewSheetModel.GetSheetAlbum(s) for s in selections])
+        sheet_numbers = set([ViewSheetModel.get_sheet_album(s) for s in selections])
         if len(sheet_numbers) > 1:
-            alert("У выделенных элементов указано несколько альбомов.")
+            forms.alert("У выделенных листов указано несколько альбомов.", exitscript=True)
 
         return selections
 
 
-
 # Проверка на соответствие выбранному альбому
-def AlbumFilter(x, album):
+def album_filter(x, album):
     if x.GetParamValueOrDefault(SharedParamsConfig.Instance.AlbumBlueprints) == album:
         return 1
     else:
@@ -223,7 +202,7 @@ def AlbumFilter(x, album):
 
 
 # Получение числа из строки
-def ClearString(msg):
+def clear_string(msg):
     number = ""
     for i in range(len(msg)):
         if msg[i].isdigit():
@@ -232,101 +211,97 @@ def ClearString(msg):
 
 
 # Получаем число для сравнения из строки вида "str-num.num"
-def GetNumber(list):
-    return float(list.pop())
+def get_number(splitted):
+    return float(splitted.pop())
 
 
-def CheckSheets(selections):
+def check_sheets(selections):
     if not selections:
         forms.alert("Не были выбраны листы.", exitscript=True)
 
-    sheet_numbers = set([ViewSheetModel.GetSheetAlbum(s) for s in selections])
+    sheet_numbers = set([ViewSheetModel.get_sheet_album(s) for s in selections])
     if len(sheet_numbers) > 1:
         forms.alert("У выделенных листов указано несколько альбомов.", exitscript=True)
 
 
-def renumber(step, direction, count, transactionName):
+def renumber(step, direction, count, transaction_name):
     doc = __revit__.ActiveUIDocument.Document
     selection = list(__revit__.ActiveUIDocument.GetSelectedElements())
     selection = [view for view in selection if view.ViewType == ViewType.DrawingSheet]
 
     # проверка выделенных элементов
-    CheckSheets(selection)
+    check_sheets(selection)
 
     # настройка атрибутов
     project_parameters = ProjectParameters.Create(__revit__.Application)
     project_parameters.SetupRevitParams(doc, SharedParamsConfig.Instance.AlbumBlueprints,
                                         SharedParamsConfig.Instance.StampSheetNumber)
 
-    # список из листов учавствующих в перестановке
-    SheetsCurrentAlbumList = []
+    # список из листов участвующих в перестановке
+    sheets_current_album_list = []
 
-    # список, на основе кторого будет сортировка
-    SortingList = []
+    # список, на основе которого будет сортировка
+    sorting_list = []
 
     # список, на основе которого будет происходить перенумерование листов
-    SheetsComposeAlbumList = []
+    sheets_compose_album_list = []
 
     # пара значений, будет хранить в себе номер крайнего листа среди рабочих
-    prop = [GetNumber(selection[0].GetParamValueOrDefault(BuiltInParameter.SHEET_NUMBER).split("-")), ""]
+    prop = [get_number(selection[0].GetParamValueOrDefault(BuiltInParameter.SHEET_NUMBER).split("-")), ""]
 
     # название альбома
     album = selection[0].GetParamValueOrDefault(SharedParamsConfig.Instance.AlbumBlueprints)
 
     # все листы проекта
-    SheetsBaseList = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Sheets).ToElements()
+    sheets_base_list = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Sheets).ToElements()
 
     # составляем список из троек значений, [пригодный для сортировки номер, сам лист, номер листа в строковой форме]
-    for sheet in SheetsBaseList:
-        if AlbumFilter(sheet, album):
-            NumberString = sheet.GetParamValueOrDefault(BuiltInParameter.SHEET_NUMBER)
-            NumberList = GetNumber(NumberString.split("-"))
-            SortingList.append([NumberList, sheet, NumberString])
+    for sheet in sheets_base_list:
+        if album_filter(sheet, album):
+            number_string = sheet.GetParamValueOrDefault(BuiltInParameter.SHEET_NUMBER)
+            number_list = get_number(number_string.split("-"))
+            sorting_list.append([number_list, sheet, number_string])
 
     # сортируем список
-    SheetsCurrentAlbumList = sorted(SortingList, cmp=ViewSheetComparer().Compare, key=lambda x: x[1])
+    sheets_current_album_list = sorted(sorting_list, cmp=ViewSheetComparer().Compare, key=lambda x: x[1])
 
     # если перемещаем вниз, то нужно перевернуть список
     if direction < 0:
-        SheetsCurrentAlbumList = SheetsCurrentAlbumList[::-1]
+        sheets_current_album_list = sheets_current_album_list[::-1]
 
     # находим опорное значение из выбранных листов, при уменьшении номера ищем большее, иначе - меньшее
     for sheet in selection:
-        NumberString = sheet.GetParamValueOrDefault(BuiltInParameter.SHEET_NUMBER)
-        NumberInt = GetNumber(NumberString.split("-"))
-        if direction * prop[0] < direction * NumberInt:
-            prop[0] = NumberInt
-            prop[1] = NumberString
+        number_string = sheet.GetParamValueOrDefault(BuiltInParameter.SHEET_NUMBER)
+        number_int = get_number(number_string.split("-"))
+        if direction * prop[0] < direction * number_int:
+            prop[0] = number_int
+            prop[1] = number_string
 
     # идем с конца списка, удаляя все листы стоящие после наших выбранных
-    for sheet in SheetsCurrentAlbumList[::-1]:
+    for sheet in sheets_current_album_list[::-1]:
         if direction * prop[0] < direction * sheet[0]:
-            SheetsCurrentAlbumList.pop()
+            sheets_current_album_list.pop()
         else:
             break
 
     # удаляем листы стоящие перед выбранными плюс step листов
-    while len(SheetsCurrentAlbumList) > count + step:
-        SheetsCurrentAlbumList.pop(0)
+    while len(sheets_current_album_list) > count + step:
+        sheets_current_album_list.pop(0)
 
     # теперь размер списка постоянный, запишем его длину
-    n = len(SheetsCurrentAlbumList)
+    n = len(sheets_current_album_list)
 
     # кладем листы в нужном порядке первым значением из пары, второе значение это номера с неизменным порядком
     for i in range(n - count, n):
-        element = [SheetsCurrentAlbumList[i - (n - count)][0], SheetsCurrentAlbumList[i][1], SheetsCurrentAlbumList[i - (n - count)][2]]
-        SheetsComposeAlbumList.append(element)
+        element = [sheets_current_album_list[i - (n - count)][0], sheets_current_album_list[i][1], sheets_current_album_list[i - (n - count)][2]]
+        sheets_compose_album_list.append(element)
     for i in range(n - count):
-        element = [SheetsCurrentAlbumList[i + count][0], SheetsCurrentAlbumList[i][1], SheetsCurrentAlbumList[i + count][2]]
-        SheetsComposeAlbumList.append(element)
+        element = [sheets_current_album_list[i + count][0], sheets_current_album_list[i][1], sheets_current_album_list[i + count][2]]
+        sheets_compose_album_list.append(element)
 
-    with Transaction(doc, transactionName) as transaction:
-        transaction.Start()
-
-        for idx, sheet in enumerate(SheetsComposeAlbumList):
+    with revit.Transaction("BIM: " + transaction_name):
+        for idx, sheet in enumerate(sheets_compose_album_list):
             sheet[1].SetParamValue(BuiltInParameter.SHEET_NUMBER, str(idx) + "+Temp")
-        for sheet in SheetsComposeAlbumList:
+        for sheet in sheets_compose_album_list:
             sheet[1].SetParamValue(BuiltInParameter.SHEET_NUMBER, sheet[2])
             sheet[1].SetParamValue(SharedParamsConfig.Instance.StampSheetNumber, str(int(sheet[0])))
-
-        transaction.Commit()
