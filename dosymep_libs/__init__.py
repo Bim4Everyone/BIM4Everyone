fdcdae884386979102b5dd8b7ea4c0a0c48725c3
Vladimir Stroganov <vlastroganov@gmail.com>
2025-01-09 10:09:15 +0300

    Bim4Everyone.lib: Добавлена функция по обновлению вкладки (#12)

--- dosymep_libs/__init__.py
+++ dosymep_libs/__init__.py
@@ -4,8 +4,12 @@ import os
 import sys
 import shutil
 import os.path as op
+import pyrevit.coreutils.git as libgit
+from pyrevit.versionmgr import updater
+from pyrevit.loader import sessionmgr
 
 from pyrevit import HOST_APP
+from pyrevit.coreutils import envvars
 
 
 def copy_files(source_dir, target_dir):
@@ -28,3 +32,22 @@ def load_assemblies():
     sys.path.append(temp_path)
 
 
+def update_extension(extension_file):
+    if not check_update_in_progress():
+        try:
+            set_autoupdate_in_progress(True)
+            path = os.path.abspath(extension_file)
+            repo_path = libgit.libgit.Repository.Discover(path)
+            repo_info = libgit.get_repo(repo_path)
+            updater.update_repo(repo_info)
+            sessionmgr.load_session()
+        finally:
+            set_autoupdate_in_progress(False)
+
+
+def check_update_in_progress():
+    return envvars.get_pyrevit_env_var(envvars.AUTOUPDATING_ENVVAR)
+
+
+def set_autoupdate_in_progress(state):
+    envvars.set_pyrevit_env_var(envvars.AUTOUPDATING_ENVVAR, state)
